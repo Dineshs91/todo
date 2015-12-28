52a482181fa5f229b23ba686bbce13e003c23137
dinesh <dineshpy07@gmail.com>
2015-12-28 20:07:36 +0530

    Validate todoId and match email token and todoId
    Url is valid if both the todoId and the email token are linked

--- routes/email.js
+++ routes/email.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Q = require('q');
 var mongoose = require('mongoose');
+ObjectId = mongoose.Types.ObjectId;
 
 var Todo = require('../models/todo').Todo;
 var EmailAction = require('../models/emailAction').EmailAction;
@@ -9,13 +10,19 @@ var EmailAction = require('../models/emailAction').EmailAction;
 mongoose.Promise = Q.Promise;
 
 // http://localhost:3000/email/todo/{todoId}/{token}/{action}
-function validateToken(token) {
+/*
+  Validation:
+    - todoId
+    - token
+    - expires
+*/
+function validateToken(todoId, token) {
   var deferred = Q.defer();
 
   // check for token and expires.
   var nowTimestamp = Date.now()
-  EmailAction.find({ $and: [{token: token }, {expires: { $lt: nowTimestamp } },
-   {stat: { $ne: 'closed' } }] }).then(function(actions) {
+  EmailAction.find({ $and: [ {todo_id: todoId}, {token: token}, {expires: { $lt: nowTimestamp } },
+  ] }).then(function(actions) {
     if(actions == undefined) {
       deferred.reject();
     } else {
@@ -28,10 +35,27 @@ function validateToken(token) {
   return deferred.promise;
 }
 
+/*
+  Validation:
+    - todo should not be closed.
+*/
+function validateTodo(todoId) {
+  var deferred = Q.defer();
+
+  var todoObjectId = ObjectId(todoId);
+  Todo.find({ $and: [ { _id: todoObjectId }, {stat: { $ne: 'closed' }} ]}).then(function() {
+    deferred.resolve(true);
+  }).catch(function(err) {
+    deferred.reject(err);
+  });
+
+  return deferred.promise;
+}
+
 // Update todo item.
 function updateTodo(todoId, action) {
   var deferred = Q.defer();
-  var todoObjectId = new mongoose.Types.ObjectId(todoId);
+  var todoObjectId = ObjectId(todoId);
   var promise;
   var newTime = new Date((Date.now() + 86400000));
 
@@ -57,7 +81,9 @@ router.get('/todo/:todoId/:token/:action', function(req, res) {
   var token = req.params.token;
   var action = req.params.action;
 
-  validateToken(token).then(function(status) {
+  validateToken(todoId, token).then(function(status) {
+    return validateTodo(todoId);
+  }).then(function(status) {
     if(status) {
       return updateTodo(todoId, action);
     }
